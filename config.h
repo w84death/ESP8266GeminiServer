b8b144cf02f5709cf132e67258e7c277b14b6334
Krzysztof Jankowski <kj@p1x.in>
2022-11-12 16:26:51 +0100

    KKJ update

--- config.h
+++ config.h
@@ -1,23 +1,25 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-#define SSID   "wifi_name"
-#define PASSWD  "wifi_password"
+#define SSID   "WIFI-NAME"
+#define PASSWD  "WIFI-PASSWORD"
 
-#define HOSTNAME  "gemini"
-#define PORT      1965
+#define HOSTNAME "gemini"
+#define PORT 1965
+#define LED 2
 
 const char server_private_key[] PROGMEM = R"EOF(
------BEGIN RSA PRIVATE KEY-----
-your key here
------END RSA PRIVATE KEY-----
+-----BEGIN PRIVATE KEY-----
+PASTE HERE CONTENT OF KEY FILE
+-----END PRIVATE KEY-----
+
 
 )EOF";
 
 // The server's public certificate which must be shared
 const char server_cert[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
-your certificate here
+PASTE HERE CONTENT OF CERT FILE
 -----END CERTIFICATE-----
 
 
--- gemserv.ino
+++ gemserv.ino
@@ -1,5 +1,7 @@
-// i'd like to begin by saying that i have no idea what i'm doing
-
+// Fork of https://github.com/Astrrra/ESP8266GeminiServer
+// Modified by Krzysztof Krystian Jankowski
+// https://krzysztofjankowski.com
+// gemini://gemini.p1x.in
 
 #include<ESP8266WiFi.h>
 #include<time.h>
@@ -21,15 +23,31 @@ static const char *HEADER_BIN_OK            = "20 application/octet-stream\r\n";
 static const char *HEADER_NOT_FOUND         = "51 File Not Found\r\n";
 static const char *HEADER_INTERNAL_FAIL     = "50 Internal Server Error\r\n";
 
+//WiFiServer server(PORT);
+
 // tls server
 BearSSL::WiFiServerSecure server(PORT);
 
 void setup()
-{   
+{
+    Serial.begin(9600);
+    delay(1000);
+    Serial.print("-------------------------\n");
+    Serial.print("#> Starting GEMINI Server\n");
+
+    // LED blinking
+    pinMode(LED, OUTPUT);
+
     // setup LittleFS
     LittleFSConfig cfg;
     cfg.setAutoFormat(false);
 
+    if(!LittleFS.begin()){
+      Serial.println("!> An Error has occurred while mounting LittleFS");
+      return;
+    }
+
+    Serial.print("*> Connecting to WiFi...");
     // connect to wifi as defined in config.h
     WiFi.mode(WIFI_STA);
     WiFi.hostname(HOSTNAME);
@@ -37,40 +55,50 @@ void setup()
 
     // wait for connection
     while(WiFi.status() != WL_CONNECTED){delay(1000);}
+    Serial.print(" connected IP: [");
+    Serial.print(WiFi.localIP());
+    Serial.print(" ]\n");
 
     // add private cert and key
     BearSSL::X509List *serverCertList = new BearSSL::X509List(server_cert);
     BearSSL::PrivateKey *serverPrivateKey = new BearSSL::PrivateKey(server_private_key);
-    
     server.setRSACert(serverCertList, serverPrivateKey);
+
+    Serial.print("*> Starting gemini server...");
 
     // begin accepting connections
     server.begin();
-
+    Serial.print("started!\n");
+    Serial.print("#> Waiting for incoming connections...\n");
 }
 
 void loop()
 {
     BearSSL::WiFiClientSecure incomingConnection = server.available();
+
     if (!incomingConnection){return;} // if no connections then do nothing
 
     while (incomingConnection.connected()){
+        Serial.print("...");
         if (incomingConnection.available()){
+            Serial.print("\n*> Client connected...\n");
+            digitalWrite(LED, LOW);
             String requestString = incomingConnection.readStringUntil('\r\n'); // get request string
             Request request = Request(requestString);
-
-            if (!LittleFS.exists(request.getFile_path())) // if file does not exist
-            { 
+            Serial.print(request.getFile_path());
+            if (!LittleFS.exists(request.getFile_path().c_str())) // if file does not exist
+            {
                 // Send file not found header
                 incomingConnection.read();
                 incomingConnection.write((uint8_t*)HEADER_NOT_FOUND, strlen(HEADER_NOT_FOUND));
                 incomingConnection.flush();
                 incomingConnection.stop();
+                Serial.print(" ...not found\n");
             }
             else // file exists
             {
                 File file = LittleFS.open(request.getFile_path().c_str(), "r"); // open file
-
+                Serial.print(" ...sending!\n");
                 if (!file.isFile()) // error opening file
                 {
                     file.close();
@@ -83,14 +111,14 @@ void loop()
                 else // file is OK
                 {
                     incomingConnection.read();  // read the rest of the incomig data
-                                                
+
                     String extension = request.getFile_ext(); // get the file extension and send the required header
-                    if (extension.equals("gmi"))        {incomingConnection.write((uint8_t*)HEADER_GEM_OK, strlen(HEADER_GEM_OK));} 
+                    if (extension.equals("gmi"))        {incomingConnection.write((uint8_t*)HEADER_GEM_OK, strlen(HEADER_GEM_OK));}
                     else if (extension.equals("txt"))   {incomingConnection.write((uint8_t*)HEADER_PLAIN_OK, strlen(HEADER_PLAIN_OK));}
-                    else if (extension.equals("md"))    {incomingConnection.write((uint8_t*)HEADER_MARKDOWN_OK, strlen(HEADER_MARKDOWN_OK));} 
-                    else if (extension.equals("jpg"))   {incomingConnection.write((uint8_t*)HEADER_JPEG_OK, strlen(HEADER_JPEG_OK));} 
+                    else if (extension.equals("md"))    {incomingConnection.write((uint8_t*)HEADER_MARKDOWN_OK, strlen(HEADER_MARKDOWN_OK));}
+                    else if (extension.equals("jpg"))   {incomingConnection.write((uint8_t*)HEADER_JPEG_OK, strlen(HEADER_JPEG_OK));}
                     else                                {incomingConnection.write((uint8_t*)HEADER_BIN_OK, strlen(HEADER_BIN_OK));}
-                    
+
                     // time to read the file itself
 
                     // calculate count of chunks and size of the stuff that didn't fit
@@ -117,6 +145,7 @@ void loop()
                     incomingConnection.stop();
                 }
             }
+            digitalWrite(LED, HIGH);
         }
     }
 }
